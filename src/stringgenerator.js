d0dd74ad90c13c4100071ada0ef457a7a5c76271
JDB <legodude17@users.noreply.github.com>
2018-10-24 16:51:59 -0700

    Deleted stuff and started conversion

--- src/stringgenerator.js
+++ src/stringgenerator.js
@@ -1,156 +1,160 @@
 /**
- * @class (Markov process)-based string generator. 
- * Copied from a <a href="http://www.roguebasin.roguelikedevelopment.org/index.php?title=Names_from_a_high_order_Markov_Process_and_a_simplified_Katz_back-off_scheme">RogueBasin article</a>. 
+ * @class (Markov process)-based string generator.
+ * Copied from http://www.roguebasin.roguelikedevelopment.org/index.php?title=Names_from_a_high_order_Markov_Process_and_a_simplified_Katz_back-off_scheme
  * Offers configurable order and prior.
  * @param {object} [options]
  * @param {bool} [options.words=false] Use word mode?
  * @param {int} [options.order=3]
  * @param {float} [options.prior=0.001]
  */
-ROT.StringGenerator = function(options) {
-	this._options = {
-		words: false,
-		order: 3,
-		prior: 0.001
-	};
-	for (var p in options) { this._options[p] = options[p]; }
+import { getWeightedValue } from './rng';
 
-	this._boundary = String.fromCharCode(0);
-	this._suffix = this._boundary;
-	this._prefix = [];
-	for (var i=0;i<this._options.order;i++) { this._prefix.push(this._boundary); }
+const StringGenerator = function StringGenerator(options) {
+  this._options = {
+    words: false,
+    order: 3,
+    prior: 0.001
+  };
+  for (const p in options) {
+    if (options.hasOwnPoroperty(p)) this._options[p] = options[p];
+  }
 
-	this._priorValues = {};
-	this._priorValues[this._boundary] = this._options.prior;
+  this._boundary = String.fromCharCode(0);
+  this._suffix = this._boundary;
+  this._prefix = [];
+  for (let i = 0; i < this._options.order; i++) { this._prefix.push(this._boundary); }
 
-	this._data = {};
+  this._priorValues = {};
+  this._priorValues[this._boundary] = this._options.prior;
+
+  this._data = {};
 };
 
 /**
  * Remove all learning data
  */
-ROT.StringGenerator.prototype.clear = function() {
-	this._data = {};
-	this._priorValues = {};
+StringGenerator.prototype.clear = function clear() {
+  this._data = {};
+  this._priorValues = {};
 };
 
 /**
  * @returns {string} Generated string
  */
-ROT.StringGenerator.prototype.generate = function() {
-	var result = [this._sample(this._prefix)];
-	while (result[result.length-1] != this._boundary) {
-		result.push(this._sample(result));
-	}
-	return this._join(result.slice(0, -1));
+StringGenerator.prototype.generate = function generate() {
+  const result = [this._sample(this._prefix)];
+  while (result[result.length - 1] != this._boundary) {
+    result.push(this._sample(result));
+  }
+  return this._join(result.slice(0, -1));
 };
 
 /**
  * Observe (learn) a string from a training set
  */
-ROT.StringGenerator.prototype.observe = function(string) {
-	var tokens = this._split(string);
+StringGenerator.prototype.observe = function observe(string) {
+  let tokens = this._split(string);
 
-	for (var i=0; i<tokens.length; i++) {
-		this._priorValues[tokens[i]] = this._options.prior;
-	}
+  for (let i = 0; i < tokens.length; i++) {
+    this._priorValues[tokens[i]] = this._options.prior;
+  }
 
-	tokens = this._prefix.concat(tokens).concat(this._suffix); /* add boundary symbols */
+  tokens = this._prefix.concat(tokens).concat(this._suffix); /* add boundary symbols */
 
-	for (var i=this._options.order; i<tokens.length; i++) {
-		var context = tokens.slice(i-this._options.order, i);
-		var event = tokens[i];
-		for (var j=0; j<context.length; j++) {
-			var subcontext = context.slice(j);
-			this._observeEvent(subcontext, event);
-		}
-	}
+  for (let i = this._options.order; i < tokens.length; i++) {
+    const context = tokens.slice(i - this._options.order, i);
+    const event = tokens[i];
+    for (let j = 0; j < context.length; j++) {
+      const subcontext = context.slice(j);
+      this._observeEvent(subcontext, event);
+    }
+  }
 };
 
-ROT.StringGenerator.prototype.getStats = function() {
-	var parts = [];
+StringGenerator.prototype.getStats = function getStats() {
+  const parts = [];
 
-	var priorCount = 0;
-	for (var p in this._priorValues) { priorCount++; }
-	priorCount--; /* boundary */
-	parts.push("distinct samples: " + priorCount);
+  let priorCount = 0;
+  for (var p in this._priorValues) { priorCount++; }
+  priorCount--; /* boundary */
+  parts.push(`distinct samples: ${priorCount}`);
 
-	var dataCount = 0;
-	var eventCount = 0;
-	for (var p in this._data) { 
-		dataCount++; 
-		for (var key in this._data[p]) {
-			eventCount++;
-		}
-	}
-	parts.push("dictionary size (contexts): " + dataCount);
-	parts.push("dictionary size (events): " + eventCount);
+  let dataCount = 0;
+  let eventCount = 0;
+  for (var p in this._data) {
+    dataCount++;
+    for (const key in this._data[p]) {
+      eventCount++;
+    }
+  }
+  parts.push(`dictionary size (contexts): ${dataCount}`);
+  parts.push(`dictionary size (events): ${eventCount}`);
 
-	return parts.join(", ");
+  return parts.join(', ');
 };
 
 /**
  * @param {string}
  * @returns {string[]}
  */
-ROT.StringGenerator.prototype._split = function(str) {
-	return str.split(this._options.words ? /\s+/ : "");
-};
-
-/**
- * @param {string[]}
- * @returns {string} 
- */
-ROT.StringGenerator.prototype._join = function(arr) {
-	return arr.join(this._options.words ? " " : "");
-};
-
-/**
- * @param {string[]} context
- * @param {string} event
- */
-ROT.StringGenerator.prototype._observeEvent = function(context, event) {
-	var key = this._join(context);
-	if (!(key in this._data)) { this._data[key] = {}; }
-	var data = this._data[key];
-
-	if (!(event in data)) { data[event] = 0; }
-	data[event]++;
+StringGenerator.prototype._split = function _split(str) {
+  return str.split(this._options.words ? /\s+/ : '');
 };
 
 /**
  * @param {string[]}
  * @returns {string}
  */
-ROT.StringGenerator.prototype._sample = function(context) {
-	context = this._backoff(context);
-	var key = this._join(context);
-	var data = this._data[key];
+StringGenerator.prototype._join = function _join(arr) {
+  return arr.join(this._options.words ? ' ' : '');
+};
 
-	var available = {};
+/**
+ * @param {string[]} context
+ * @param {string} event
+ */
+StringGenerator.prototype._observeEvent = function _observeEvent(context, event) {
+  const key = this._join(context);
+  if (!(key in this._data)) { this._data[key] = {}; }
+  const data = this._data[key];
 
-	if (this._options.prior) {
-		for (var event in this._priorValues) { available[event] = this._priorValues[event]; }
-		for (var event in data) { available[event] += data[event]; }
-	} else { 
-		available = data;
-	}
+  if (!(event in data)) { data[event] = 0; }
+  data[event]++;
+};
 
-	return ROT.RNG.getWeightedValue(available);
+/**
+ * @param {string[]}
+ * @returns {string}
+ */
+StringGenerator.prototype._sample = function _sample(context) {
+  context = this._backoff(context);
+  const key = this._join(context);
+  const data = this._data[key];
+
+  let available = {};
+
+  if (this._options.prior) {
+    for (var event in this._priorValues) { available[event] = this._priorValues[event]; }
+    for (var event in data) { available[event] += data[event]; }
+  } else {
+    available = data;
+  }
+
+  return getWeightedValue(available);
 };
 
 /**
  * @param {string[]}
  * @returns {string[]}
  */
-ROT.StringGenerator.prototype._backoff = function(context) {
-	if (context.length > this._options.order) {
-		context = context.slice(-this._options.order);
-	} else if (context.length < this._options.order) {
-		context = this._prefix.slice(0, this._options.order - context.length).concat(context);
-	}
+StringGenerator.prototype._backoff = function _backoff(context) {
+  if (context.length > this._options.order) {
+    context = context.slice(-this._options.order);
+  } else if (context.length < this._options.order) {
+    context = this._prefix.slice(0, this._options.order - context.length).concat(context);
+  }
 
-	while (!(this._join(context) in this._data) && context.length > 0) { context = context.slice(1); }
+  while (!(this._join(context) in this._data) && context.length > 0) { context = context.slice(1); }
 
-	return context;
+  return context;
 };
